8bf7a48f3672fec3724bed8b8b710f03588595f3
lestrrat <lestrrat+github@gmail.com>
2009-12-04 15:18:09 +0900

    version++

--- lib/Orochi.pm
+++ lib/Orochi.pm
@@ -11,7 +11,7 @@ use namespace::clean -except => qw(meta);
 
 use constant DEBUG => ($ENV{OROCHI_DEBUG} || 0);
 
-our $VERSION = '0.00005';
+our $VERSION = '0.00006';
 
 has prefix => (
     is => 'ro',
